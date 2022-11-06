34e4cb120b159706cd023cb8be82b900f078161e
laurentDellaNegra <laurent.dellanegra@gmail.com>
2022-11-06 21:23:08 +0100

    ✨ Splitting API to improve tracking progress

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,57 @@ app.get('/status', async (req, res) => {
   res.end()
 })
 
+// 1. Load products from Shopify
+app.get('/export-shopify-products', async (req, res) => {
+  try {
+    console.log('[SHOPIFY] Exporting products...')
+    const products = await shopify.getProducts()
+    console.log(`[SHOPIFY] ${products.length} products loaded !`)
+
+    res.send(products)
+  } catch (error) {
+    console.log('error', error)
+    res.status(500)
+    res.send(error.message)
+  }
+  res.end()
+})
+
+// 2. Export them into Algolia
+app.get('/import-algolia-products', async (req, res) => {
+  try {
+    const products = req.body.products
+    console.log('[ALGOLIA] Importing products...')
+    const ids = await algolia.saveProducts(products)
+    console.log(`[ALGOLIA] ${ids.length} records created !`)
+
+    res.send(ids)
+  } catch (error) {
+    console.log('error', error)
+    res.status(500)
+    res.send(error.message)
+  }
+  res.end()
+})
+
+// 3. Deploy website with vercel
+app.get('/deploy-vercel-terre-rouge-web', async (req, res) => {
+  try {
+    console.log('[VERCEL] Starting deployment...')
+    const response = await vercel.deploy()
+    console.log('[VERCEL] New deployment triggered !')
+
+    res.send(response)
+  } catch (error) {
+    console.log('error', error)
+    res.status(500)
+    res.send(error.message)
+  }
+  res.end()
+})
+
 // Deploy website
-app.get('/deploy', async (req, res) => {
+app.get('/deploy-all', async (req, res) => {
   try {
     // 1. Load products from Shopify
     console.log('[SHOPIFY] Exporting products...')
